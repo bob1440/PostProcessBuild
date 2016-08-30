679fe2d5941e8d9a9c7868330db14af5fdea6016
takumi ninomiya <ninomiya@cs-reporters.com>
2016-08-30 14:18:27 +0900

    Unity 5.3.4 Bitcode対策

---
+++ PostProcessBuild.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.Collections;
+
+public static class PostProcessBuild {
+
+	[PostProcessBuild(999)]
+	public static void OnPostProcessBuild( BuildTarget buildTarget, string path)
+	{
+		if(buildTarget == BuildTarget.iOS)
+		{
+			string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+			PBXProject pbxProject = new PBXProject();
+			pbxProject.ReadFromFile(projectPath);
+
+			string target = pbxProject.TargetGuidByName("Unity-iPhone");            
+			pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+
+			pbxProject.WriteToFile (projectPath);
+		}
+	}
+}
