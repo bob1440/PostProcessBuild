7fa6be5952a2a2816d220b0da4a05e22c86da926
takumi ninomiya <ninomiya@cs-reporters.com>
2016-08-31 16:41:27 +0900

    フレームワーク自動追加スクリプトを追加

--- PostProcessBuild.cs
+++ PostProcessBuild.cs
@@ -16,8 +16,22 @@ public static class PostProcessBuild {
 			PBXProject pbxProject = new PBXProject();
 			pbxProject.ReadFromFile(projectPath);
 
-			string target = pbxProject.TargetGuidByName("Unity-iPhone");            
+			string target = pbxProject.TargetGuidByName("Unity-iPhone");
+			
+			// ▽▽▽▽
+			            
 			pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+			
+			//pbxProject.AddFrameworkToProject (target, "AssetsLibrary.framework", true);
+			//CopyAndReplaceDirectory("Assets/Lib/mylib.framework", Path.Combine(path, "Frameworks/mylib.framework"));
+            //proj.AddFileToBuild(target, proj.AddFile("Frameworks/mylib.framework", "Frameworks/mylib.framework", PBXSourceTree.Source));
+            //ファイルを追加
+           	//var fileName = "my_file.xml";
+            //var filePath = Path.Combine("Assets/Lib", fileName);
+            //File.Copy(filePath, Path.Combine(path, fileName));
+            //proj.AddFileToBuild(target, proj.AddFile(fileName, fileName, PBXSourceTree.Source));
+
+			// △△△△△
 
 			pbxProject.WriteToFile (projectPath);
 		}
